6b6bc81d66639dd6938436c67781415b3776823b
iasoloteravision <iasolo@teravisiontech.com>
2023-10-03 09:21:07 -0500

    first commit

---
+++ Home_page.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Code Codix FAQ",
+    page_icon="👋",
+)
+
+st.success("# Welcome to Code Codix FAQ page! ")
+
+st.warning("What is a Chatbot?")
+st.info("A chatbot is a computer program that simulates human conversation through voice commands or text chats or both.")
+
+st.warning("How do I create a product with you?")
+st.info("If you have an idea contact us by our online form , e-mail or phone . We'll meet and talk it over. Just be sure to prepare as much info about your idea as possible, it will smoothen the meeting and benefit further cooperation.")
+
+st.warning("How much do you charge?")
+st.info("It depends on many things: project scope, time spent on the project and technological issues. We know it sounds vague but its simply honest. Remember that each software project is different, especially if you had a particularly original idea. We do not want to give you an unrealistic estimate, instead we want to provide you with the best quality possible within your budget.")
+
+st.warning("What is your experience?")
+st.info("We have more than 10 years of business experience. We've worked with and completed projects for leading retailers, international food and drink companies, start-ups, real estate, telecommunications, fashion companies, and many, many more.")
+
+st.warning("Do you provide any services apart from Software Development?")
+st.info("Yes, we can also help you with Product Design and Digital Transformation Strategy.")
+
+
